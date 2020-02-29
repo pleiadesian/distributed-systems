ef07eabc9b88ae8424d4b12ccb83ccf862e59813
pleiadesian <wzl574402791@outlook.com>
2020-02-29 13:14:27 +0800

    [pre-lab] finish send and receive

--- pre-lab/rdt/rdt_receiver.cc
+++ pre-lab/rdt/rdt_receiver.cc
@@ -1,16 +1,13 @@
 /*
  * FILE: rdt_receiver.cc
  * DESCRIPTION: Reliable data transfer receiver.
- * NOTE: This implementation assumes there is no packet loss, corruption, or 
- *       reordering.  You will need to enhance it to deal with all these 
- *       situations.  In this implementation, the packet format is laid out as 
+ * NOTE: In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-             the rest            ->|
- *       | payload size |<-             payload             ->|
- *
- *       The first byte of each packet indicates the size of the payload
- *       (excluding this single-byte header)
+ *       |<-        1 byte        ->|
+ *       |<-      packet info     ->|
+ *       |<-1 bit->|<-    7 bit   ->|<-    4 byte    ->|<-             the rest            ->|
+ *       |<- EOM ->|<-payload size->|<-  seq/ack no. ->|<-             payload             ->|
  */
 
 
@@ -21,11 +18,31 @@
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
 
+#define MAX_MESSAGE_SIZE    10000
+#define INFO_OFFSET         0
+#define INFO_SIZE           1
+#define SEQNUM_OFFSET       INFO_OFFSET + INFO_SIZE
+#define SEQNUM_SIZE         sizeof(int)
+#define PAYLOAD_OFFSET      SEQNUM_OFFSET + SEQNUM_SIZE
+
+#define END_OF_MESSAGE(data)    ((data[INFO_OFFSET] & 0X80) >> 7)
+#define SEQ_NO(data)                                                           \
+  {                                                                            \
+    (data[SEQNUM_OFFSET] << 24 | data[SEQNUM_OFFSET + 1] << 16 |               \
+     data[SEQNUM_OFFSET + 2] << 8 | data[SEQNUM_OFFSET + 3])                   \
+  }
+#define PAYLOAD_SIZE(data)      (data[INFO_OFFSET] & 0X7F)
+
+static char *curr_msg_data;             // current message to be constructed
+static int curr_msg_size = 0;           // current message size
+static int seq_expected = 0;            // next seq expected inbound
 
 /* receiver initialization, called once at the very beginning */
 void Receiver_Init()
 {
     fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
+    curr_msg_data = (char *)malloc(MAX_MESSAGE_SIZE);
+    ASSERT(curr_msg_data != NULL);
 }
 
 /* receiver finalization, called once at the very end.
@@ -35,31 +52,43 @@ void Receiver_Init()
 void Receiver_Final()
 {
     fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
+    free(curr_msg_data);
 }
 
 /* event handler, called when a packet is passed from the lower layer at the 
    receiver */
 void Receiver_FromLowerLayer(struct packet *pkt)
 {
-    /* 1-byte header indicating the size of the payload */
-    int header_size = 1;
+    /* if sequence number is not expected, discard it */
+    int seq_no = SEQ_NO(pkt->data);
+    if (seq_no != seq_expected) 
+        return;
+    
+    /* concatenate the new data into current constructing message */
+    ASSERT(PAYLOAD_SIZE(pkt->data) > 0 && PAYLOAD_SIZE(pkt->data) <= RDT_PKTSIZE - SEQNUM_SIZE - INFO_SIZE);
+    memcpy(curr_msg_data + curr_msg_size, pkt->data + PAYLOAD_OFFSET, PAYLOAD_SIZE(pkt->data));
+    curr_msg_size += PAYLOAD_SIZE(pkt->data);
 
-    /* construct a message and deliver to the upper layer */
-    struct message *msg = (struct message*) malloc(sizeof(struct message));
-    ASSERT(msg!=NULL);
+    /* finish constructing a message, send it to upper layer */
+    if (END_OF_MESSAGE(pkt->data)) {
+        struct message *msg = (struct message*)malloc(sizeof(struct message));
+        ASSERT(msg != NULL);
+        msg->data = (char *)malloc(curr_msg_size);
+        ASSERT(msg->data != NULL);
+        msg->size = curr_msg_size;
+        memcpy(msg->data, curr_msg_data, curr_msg_size);
+        Receiver_ToUpperLayer(msg);
+        if (msg->data != NULL) 
+            free(msg->data);
+        if (msg != NULL) 
+            free(msg);
+        curr_msg_size = 0;
+        memset(curr_msg_data, '\0', curr_msg_size);
+    }
 
-    msg->size = pkt->data[0];
-
-    /* sanity check in case the packet is corrupted */
-    if (msg->size<0) msg->size=0;
-    if (msg->size>RDT_PKTSIZE-header_size) msg->size=RDT_PKTSIZE-header_size;
-
-    msg->data = (char*) malloc(msg->size);
-    ASSERT(msg->data!=NULL);
-    memcpy(msg->data, pkt->data+header_size, msg->size);
-    Receiver_ToUpperLayer(msg);
-
-    /* don't forget to free the space */
-    if (msg->data!=NULL) free(msg->data);
-    if (msg!=NULL) free(msg);
+    /* acknowledge the sender */
+    struct packet *ack_pkt = (struct packet *)malloc(sizeof(struct packet));
+    memset(ack_pkt->data, '\0', RDT_PKTSIZE);
+    memcpy(ack_pkt->data + SEQNUM_OFFSET, &seq_no, SEQNUM_SIZE);
+    Receiver_ToLowerLayer(ack_pkt);
 }
--- pre-lab/rdt/rdt_sender.cc
+++ pre-lab/rdt/rdt_sender.cc
@@ -1,16 +1,11 @@
 /*
  * FILE: rdt_sender.cc
  * DESCRIPTION: Reliable data transfer sender.
- * NOTE: This implementation assumes there is no packet loss, corruption, or 
- *       reordering.  You will need to enhance it to deal with all these 
- *       situations.  In this implementation, the packet format is laid out as 
+ * NOTE: In this implementation, the packet format is laid out as 
  *       the following:
  *       
- *       |<-  1 byte  ->|<-             the rest            ->|
- *       | payload size |<-             payload             ->|
- *
- *       The first byte of each packet indicates the size of the payload
- *       (excluding this single-byte header)
+ *       |<-1 bit->|<-    7 bit   ->|<-  4 byte  ->|<-             the rest            ->|
+ *       |<- EOM ->|<-payload size->|<-  seq no. ->|<-             payload             ->|
  */
 
 
@@ -21,11 +16,45 @@
 #include "rdt_struct.h"
 #include "rdt_sender.h"
 
+#define WINDOW_SIZE     10
+#define MSG_BUFFER_SIZE 15000
+#define INFO_OFFSET     0
+#define INFO_SIZE       1
+#define SEQNUM_OFFSET   INFO_OFFSET + INFO_SIZE
+#define SEQNUM_SIZE     sizeof(int)
+#define PAYLOAD_OFFSET  SEQNUM_OFFSET + SEQNUM_SIZE
+#define PAYLOAD_SIZE    RDT_PKTSIZE - SEQNUM_SIZE - INFO_SIZE
+#define TIMEOUT         0.3
+#define CHECK_INTERVAL  0.1
+
+#define END_OF_MESSAGE  0x80
+
+struct window {
+    double send_time;
+    struct packet pkt;
+};
+
+static message *msg_buffer;             // buffers for the waiting message
+static window *buffer;                  // buffers for the outbound stream
+static int ack_expected = 0;            // next ack expected inbound
+static int next_packet_to_send = 0;     // next packet going out
+static int seq_no = 0;                  // next sequence number to use
+static int nbuffer = 0;                 // number of output buffers currently in use
+static int next_message_to_send = 0;    // next message going out
+static int message_cursor = 0;          // next packet in the message going out
+static int msg_no = 0;                  // next message number to use
+static int nmsg = 0;                    // number of message buffers currently in use
+static bool sending = false;            // if sending has started
+
 
 /* sender initialization, called once at the very beginning */
 void Sender_Init()
 {
     fprintf(stdout, "At %.2fs: sender initializing ...\n", GetSimulationTime());
+    msg_buffer = (struct message *)malloc(MSG_BUFFER_SIZE * sizeof(struct message));
+    buffer = (struct window *)malloc(WINDOW_SIZE * sizeof(struct window));
+    ASSERT(msg_buffer != NULL);
+    ASSERT(buffer != NULL);
 }
 
 /* sender finalization, called once at the very end.
@@ -35,56 +64,112 @@ void Sender_Init()
 void Sender_Final()
 {
     fprintf(stdout, "At %.2fs: sender finalizing ...\n", GetSimulationTime());
+    Sender_StopTimer();
+    for (int i = 0; i < MSG_BUFFER_SIZE; i++) {
+        if (msg_buffer[i].size > 0) 
+            free(msg_buffer[i].data);
+    }
+    free(msg_buffer);
+    free(buffer);
+}
+
+/* fragmentation and move packets into window */
+void Sender_ExpandWindow()
+{
+    ASSERT(next_message_to_send < msg_no);
+    struct message *msg = &msg_buffer[next_message_to_send % MSG_BUFFER_SIZE];
+    int packet_num = msg->size / PAYLOAD_SIZE;
+    int last_packet_size = msg->size % PAYLOAD_SIZE;
+    for (int i = message_cursor; i < packet_num; i++) {
+        packet *p = &buffer[seq_no % WINDOW_SIZE].pkt;
+        char header = ~END_OF_MESSAGE & PAYLOAD_SIZE;
+        memcpy(p->data + INFO_OFFSET, &header, INFO_SIZE);
+        memcpy(p->data + SEQNUM_OFFSET, &seq_no, SEQNUM_SIZE);
+        memcpy(p->data + PAYLOAD_OFFSET, msg->data + PAYLOAD_SIZE * i, PAYLOAD_SIZE);
+        seq_no++;
+        nbuffer++;
+        message_cursor++;
+        /* stop moving packets into window when window is full*/
+        if (nbuffer >= WINDOW_SIZE) 
+            return;
+    }
+
+    /* fill last packet */
+    if (last_packet_size > 0) {
+        ASSERT(last_packet_size < 128);
+        packet *p = &buffer[seq_no % WINDOW_SIZE].pkt;
+        char header = END_OF_MESSAGE | last_packet_size;
+        memcpy(p->data + INFO_OFFSET, &header, INFO_SIZE);
+        memcpy(p->data + SEQNUM_OFFSET, &seq_no, SEQNUM_SIZE);
+        memcpy(p->data + PAYLOAD_OFFSET, msg->data + PAYLOAD_SIZE * packet_num, last_packet_size);
+        seq_no++;
+        nbuffer++;
+    } else {
+        /* mark end of message on last packet */
+        int last_seq_no = seq_no - 1;
+        packet *p = &buffer[last_seq_no % WINDOW_SIZE].pkt;
+        char header = END_OF_MESSAGE | PAYLOAD_SIZE;
+        memcpy(p->data + INFO_OFFSET, &header, INFO_SIZE);
+    }
+    nmsg--;
+    next_message_to_send++;
+}
+
+/* send all packets */
+void Sender_SendPacket()
+{
+    while (next_packet_to_send < seq_no) {
+        buffer[next_packet_to_send % WINDOW_SIZE].send_time = GetSimulationTime();
+        Sender_ToLowerLayer(&buffer[next_packet_to_send % WINDOW_SIZE].pkt);
+        next_packet_to_send++;
+    }
 }
 
 /* event handler, called when a message is passed from the upper layer at the 
    sender */
 void Sender_FromUpperLayer(struct message *msg)
 {
-    /* 1-byte header indicating the size of the payload */
-    int header_size = 1;
+    ASSERT(msg->size > 0);
+    ASSERT(nmsg < MSG_BUFFER_SIZE);
+    msg_buffer[msg_no % MSG_BUFFER_SIZE].size = msg->size;
+    msg_buffer[msg_no % MSG_BUFFER_SIZE].data = (char *)malloc(msg->size);
+    ASSERT(msg_buffer[msg_no % MSG_BUFFER_SIZE].data != NULL);
+    memcpy(&msg_buffer[msg_no].data, msg->data, msg->size);
+    msg_no++;
+    nmsg++;
 
-    /* maximum payload size */
-    int maxpayload_size = RDT_PKTSIZE - header_size;
+    Sender_ExpandWindow();
+    Sender_SendPacket();
 
-    /* split the message if it is too big */
-
-    /* reuse the same packet data structure */
-    packet pkt;
-
-    /* the cursor always points to the first unsent byte in the message */
-    int cursor = 0;
-
-    while (msg->size-cursor > maxpayload_size) {
-	/* fill in the packet */
-	pkt.data[0] = maxpayload_size;
-	memcpy(pkt.data+header_size, msg->data+cursor, maxpayload_size);
-
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
-
-	/* move the cursor */
-	cursor += maxpayload_size;
-    }
-
-    /* send out the last packet */
-    if (msg->size > cursor) {
-	/* fill in the packet */
-	pkt.data[0] = msg->size-cursor;
-	memcpy(pkt.data+header_size, msg->data+cursor, pkt.data[0]);
-
-	/* send it out through the lower layer */
-	Sender_ToLowerLayer(&pkt);
-    }
+    if (!sending) 
+        Sender_StartTimer(CHECK_INTERVAL);
 }
 
 /* event handler, called when a packet is passed from the lower layer at the 
    sender */
 void Sender_FromLowerLayer(struct packet *pkt)
 {
+    int ack_no;
+    memcpy(&ack_no, pkt->data + SEQNUM_OFFSET, SEQNUM_SIZE);
+    ASSERT(ack_no < seq_no);
+    /* ignore old acknowledge number and increase expected acknowledge number */
+    if (ack_no >= ack_expected) {
+        nbuffer -= ack_no - ack_expected + 1;
+        ack_expected = ack_no + 1;
+        /* message buffer should be drained when slots in the send window become available */
+        Sender_ExpandWindow();
+        Sender_SendPacket();
+    }
 }
 
 /* event handler, called when the timer expires */
 void Sender_Timeout()
 {
+    /* check if the first packet waiting for ack is timout */
+    if (GetSimulationTime() - buffer[ack_expected % WINDOW_SIZE].send_time >= TIMEOUT) {
+        next_packet_to_send = ack_expected;
+        Sender_ExpandWindow();
+        Sender_SendPacket();
+    }
+    Sender_StartTimer(CHECK_INTERVAL);
 }
