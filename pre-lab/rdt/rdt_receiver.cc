d320db93619d34b8ac6969348d18ad0140f095cc
pleiadesian <wzl574402791@outlook.com>
2020-03-01 10:16:27 +0800

    [pre-lab] finish Go-Back-N with receiver's buffer

--- pre-lab/rdt/rdt_receiver.cc
+++ pre-lab/rdt/rdt_receiver.cc
@@ -18,6 +18,7 @@
 #include "rdt_struct.h"
 #include "rdt_receiver.h"
 
+#define WINDOW_SIZE         10
 #define MAX_MESSAGE_SIZE    10000
 #define CHECKSUM_OFFSET     0
 #define CHECKSUM_SIZE       2
@@ -32,6 +33,12 @@
 #define PAYLOAD_SIZE(data)      (data[INFO_OFFSET] & 0X7F)
 #define SEQ_NO(data)            (*(int *)&data[SEQNUM_OFFSET])
 
+struct window {
+    bool valid;
+    struct packet pkt;
+};
+
+static struct window *buffer;           // buffers for the inbound stream
 static char *curr_msg_data;             // current message to be constructed
 static int curr_msg_size = 0;           // current message size
 static int seq_expected = 0;            // next seq expected inbound
@@ -53,7 +60,10 @@ void Receiver_Init()
 {
     fprintf(stdout, "At %.2fs: receiver initializing ...\n", GetSimulationTime());
     curr_msg_data = (char *)malloc(MAX_MESSAGE_SIZE);
+    buffer = (struct window *)malloc(WINDOW_SIZE * sizeof(struct window));
     ASSERT(curr_msg_data != NULL);
+    ASSERT(buffer != 0);
+    memset(buffer, '\0', WINDOW_SIZE * sizeof(struct window));
 }
 
 /* receiver finalization, called once at the very end.
@@ -63,33 +73,15 @@ void Receiver_Init()
 void Receiver_Final()
 {
     fprintf(stdout, "At %.2fs: receiver finalizing ...\n", GetSimulationTime());
+    free(buffer);
     free(curr_msg_data);
 }
 
-void Receiver_SendAck(int ack_no)
+/* move data from window into message buffer, return the lastest seq received */
+static int Receiver_ShrinkWindow(int seq_no)
 {
-    struct packet *ack_pkt = (struct packet *)malloc(sizeof(struct packet));
-    memset(ack_pkt->data, '\0', RDT_PKTSIZE);
-    memcpy(ack_pkt->data + SEQNUM_OFFSET, &ack_no, SEQNUM_SIZE);
-    short ack_chksum = checksum(ack_pkt);
-    memcpy(ack_pkt->data + CHECKSUM_OFFSET, &ack_chksum, CHECKSUM_SIZE);
-    Receiver_ToLowerLayer(ack_pkt);
-}
-
-/* event handler, called when a packet is passed from the lower layer at the 
-   receiver */
-void Receiver_FromLowerLayer(struct packet *pkt)
-{
-    /* if checksum does not match, discard it */
-    short chksum_expected = checksum(pkt);
-    short chksum = CHECKSUM(pkt->data);
-    if (chksum != chksum_expected) 
-        return;
-    
-    /* if sequence number is not expected, discard it */
-    if (SEQ_NO(pkt->data) > seq_expected) 
-        return;
-    else if (SEQ_NO(pkt->data) == seq_expected) {
+    while (buffer[seq_no % WINDOW_SIZE].valid) {
+        struct packet *pkt = &buffer[seq_no % WINDOW_SIZE].pkt;
         /* concatenate the new data into current constructing message */
         ASSERT(PAYLOAD_SIZE(pkt->data) > 0 && PAYLOAD_SIZE(pkt->data) <= RDT_PKTSIZE - SEQNUM_SIZE - INFO_SIZE - CHECKSUM_SIZE);
         memcpy(curr_msg_data + curr_msg_size, pkt->data + PAYLOAD_OFFSET, PAYLOAD_SIZE(pkt->data));
@@ -112,12 +104,51 @@ void Receiver_FromLowerLayer(struct packet *pkt)
             curr_msg_size = 0;
             memset(curr_msg_data, '\0', curr_msg_size);
         }
+        buffer[seq_no % WINDOW_SIZE].valid = false;
+        seq_no++;
+    }
+    return --seq_no;
+}
 
-        /* acknowledge the sender */
-        Receiver_SendAck(seq_expected);
-        seq_expected++;
+/* send ack packet */
+static void Receiver_SendAck(int ack_no)
+{
+    struct packet *ack_pkt = (struct packet *)malloc(sizeof(struct packet));
+    memset(ack_pkt->data, '\0', RDT_PKTSIZE);
+    memcpy(ack_pkt->data + SEQNUM_OFFSET, &ack_no, SEQNUM_SIZE);
+    short ack_chksum = checksum(ack_pkt);
+    memcpy(ack_pkt->data + CHECKSUM_OFFSET, &ack_chksum, CHECKSUM_SIZE);
+    Receiver_ToLowerLayer(ack_pkt);
+}
+
+/* event handler, called when a packet is passed from the lower layer at the 
+   receiver */
+void Receiver_FromLowerLayer(struct packet *pkt)
+{
+    /* if checksum does not match, discard it */
+    short chksum_expected = checksum(pkt);
+    short chksum = CHECKSUM(pkt->data);
+    if (chksum != chksum_expected) 
+        return;
+    
+    int seq_no = SEQ_NO(pkt->data);
+    if (seq_no > seq_expected) {
+        if (seq_no < seq_expected + WINDOW_SIZE) {
+            buffer[seq_no % WINDOW_SIZE].valid = true;
+            memcpy(&buffer[seq_no % WINDOW_SIZE].pkt, pkt, sizeof(struct packet));
+        } else {
+            /* if sequence number is not expected, discard it */
+            return;
+        }
+    } else if (seq_no == seq_expected) {
+        buffer[seq_no % WINDOW_SIZE].valid = true;
+        memcpy(&buffer[seq_no % WINDOW_SIZE].pkt, pkt, sizeof(struct packet));
+
+        int ack_no = Receiver_ShrinkWindow(seq_no);
+        Receiver_SendAck(ack_no);
+        seq_expected = ack_no + 1;
     } else {
         /* receive an old packets, resend ack */
-        Receiver_SendAck(SEQ_NO(pkt->data));
+        Receiver_SendAck(seq_no);
     }
 }
